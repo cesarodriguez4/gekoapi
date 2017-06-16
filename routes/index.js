b4e7c8c1e9a10d2c6804fafde1500b69d9406bb8
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-06-16 16:47:45 -0400

    .

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const nodemailer = require('nodemailer');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
-});
-
-router.post('/email', (req, res) => {
-  let mail = {
-  	from: 'soporteagronacional@gmail.com', 
-  	to: req.body.email, 
-  	subject: 'Ticket'
-  };
-
-  transporter.sendMail(mail, (error, info) => {
-    if (error) {
-    	return res.send(error);
-    }
-    return res.send(info);
-  });
 });
 
 module.exports = router;
