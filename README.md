c8121834454c7aaa9957872f49e3b5bfcb45058f
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-06-16 15:23:18 -0400

    nuevo

--- README.md
+++
@@ -1,2 +0,0 @@
-# gekoapi
-Api para gekosupplies
--- routes/servicios.js
+++
@@ -1,9 +0,0 @@
-const sql  = require('sql-crud');
-const crud = new sql("mysql");
-
-module.exports = (app, con) => {
-  app.post('/query', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
-  });
-};
