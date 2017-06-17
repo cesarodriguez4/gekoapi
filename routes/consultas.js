28cd5a8fb4532fb27fb558d6b3787d52169891f2
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-06-16 23:48:58 -0400

    auto

---
+++ routes/consultas.js
@@ -0,0 +1,15 @@
+const sql = require('sql-crud');
+let crud = new sql('mysql');
+module.exports = (app, con) => {
+	app.post('/query', (req, res) => {
+		crud.insert(con, {
+			insertInto: 'CONSULTAS',
+			values: req.body
+		}, (err, result) => {
+			if (err) {
+				res.send(error);
+			}
+			res.send(result);
+		}, true);
+	});
+}
