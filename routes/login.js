30584d797444a77c77c32e3a550777ffe8f68024
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-06-17 01:47:08 -0400

    auto

--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,22 @@
 const sql = require('sql-crud');
 let crud = new sql('mysql');
 module.exports = (app, con) => {
-	app.post('/login', (req, res) => {
-		const correo = req.body.correo;
-		const password = req.body.password;
-		crud.select(con, {
-			select: '*',
-			from: 'ADMINISTRADORES',
-			where: {correo, password}
-		}, (error, result) => {
-			if (error) {
-				res.send(error);
-			}
-			if (result.length > 0 ) {
-				res.send({success: true});
-			}
-			res.send({error: 'Este usuario no existe.'});
-		}, true);
-	});
+
+app.post('/login', (req, res) => {
+    const correo = req.body.correo;
+    const password = req.body.password;
+    crud.select(con, {
+      select: '*',
+      from: 'ADMINISTRADORES',
+      where: {correo, password}
+    }, (error, result) => {
+      if (error) {
+        res.send(error);
+      }
+      if (result.length > 0 ) {
+        res.send({success: true});
+      }
+      res.send({error: 'Correo y/o contraseña inválidas.'});
+    }, true);
+  });
 }
