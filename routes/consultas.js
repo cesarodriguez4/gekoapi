3d33140c1538ba1877313113efb9e623b7e662ad
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-06-17 00:35:13 -0400

    auto

--- routes/consultas.js
+++ routes/consultas.js
@@ -1,7 +1,9 @@
 const sql = require('sql-crud');
 let crud = new sql('mysql');
+let random = require('randomstring');
 module.exports = (app, con) => {
 	app.post('/query', (req, res) => {
+		req.body.ticket = random.generate(6).toUpperCase();
 		crud.insert(con, {
 			insertInto: 'CONSULTAS',
 			values: req.body
