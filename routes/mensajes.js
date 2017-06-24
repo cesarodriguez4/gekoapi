4b2b0e804efbe82b3be11c9822f56e4b698959f3
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-06-24 15:54:57 -0400

    auto

--- routes/mensajes.js
+++ routes/mensajes.js
@@ -3,32 +3,29 @@ let crud = new sql('mysql');
 let random = require('randomstring');
 module.exports = (app, con, transporter) => {
 	
-	app.get('/messages/:ticket', (req, res) => {
-      const ticket = req.params.ticket;
-      crud.select(con, {
-      	select: '*',
-      	from: 'MENSAJES',
-      	where: {ticket}
-      }, (err, result) => {
-      	if (err) {
-      		res.send(err);
-      	}
-      	res.send(result);
-      }, true);
-	});
+app.get('/messages/:ticket', (req, res) => {
+  const ticket = req.params.ticket;
+  const query = `SELECT * FROM MENSAJES INNER JOIN CONSULTAS ON MENSAJES.ticket = CONSULTAS.ticket WHERE ticket = ${ticket};`;
+  console.log(query);
+  con.query(query, (err, response) => {
+  if (err) {
+    res.send(err);
+  }
+    res.send(response);
+  });
+});
 
-	app.get('/users/delete/:ticket', (req, res) => {
-      const ticket = req.params.ticket;
-      console.log(req.body);
-      crud.delete(con, {
-      	from: 'CONSULTAS',
-      	where: {ticket}
-      }, 0, 1);
-      crud.delete(con, {
-      	from: 'MENSAJES',
-      	where: {ticket}
-      }, 0, 1);
-      res.send(200);
-	});
+app.get('/users/delete/:ticket', (req, res) => {
+  const ticket = req.params.ticket;
+  crud.delete(con, {
+	from: 'CONSULTAS',
+	where: {ticket}
+}, 0, 1);
+  crud.delete(con, {
+    from: 'MENSAJES',
+    where: {ticket}
+  }, 0, 1);
+  res.send(200);
+});
 
 };
